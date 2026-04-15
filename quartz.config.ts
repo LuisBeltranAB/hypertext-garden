9c5c2f7da80d6e97b3cbebb3b6c0e1127325553d
Machaiquen <lebm2000@outlook.com>
2026-04-14 20:16:26 -0600

    Quartz sync: Apr 14, 2026, 8:16 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,14 +1,9 @@
 import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
 
-/**
- * Quartz 4 Configuration
- *
- * See https://quartz.jzhao.xyz/configuration for more information.
- */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Luis Beltrán",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -29,10 +24,10 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          light: "#fdf5e6",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
+          darkgray: "#3e2723",
           dark: "#2b2b2b",
           secondary: "#284b63",
           tertiary: "#84a59d",
